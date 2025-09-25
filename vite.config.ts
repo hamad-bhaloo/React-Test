41f711f53a90d2f07f9d79ddcdddd9b848439800
hamad-bhaloo <hamadshahid80@gmail.com>
2025-09-25 18:02:01 +0500

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 5147,
+    port: 5173,
     cors: true,
     headers: {
       // Security headers
